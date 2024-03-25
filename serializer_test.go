5acea48c5988965bea4865178b31f35335dcb63d
tomprogramuje <tom.severa@outlook.cz>
2024-03-25 11:06:28 +0100

    [RED] added second []string case

--- serializer_test.go
+++ serializer_test.go
@@ -14,6 +14,7 @@ func TestSerialize(t *testing.T) {
 		{"'OK' gets converted to `+OK\r\n`", "OK", []string{`+OK\r\n`}},
 		{"'hello world' gets converted to `+hello world\r\n`", "hello world", []string{`+hello world\r\n`}},
 		{"`[]string{'ping'}` gets converted to '*1\r\n$4\r\nping\r\n'", []string{"ping"}, []string{`*1\r\n$4\r\nping\r\n`}},
+		{"'[]string{'echo', 'hello world'}' gets converted to `*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n`", []string{"echo", "hello world"}, []string{`*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n`}},
 	}
 
 	for _, test := range cases {

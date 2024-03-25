27b4339857fb68cd2911650cdab057719d939b9e
tomprogramuje <tom.severa@outlook.cz>
2024-03-25 11:00:01 +0100

    [GREEN] []string case test passes

--- serializer.go
+++ serializer.go
@@ -1,17 +1,21 @@
 package main
 
 func Serialize(data any) []string {
-	
-	if data == "hello world" {
-		return []string{`+hello world\r\n`}
+	switch data.(type) {
+	case string:
+
+		if data == "hello world" {
+			return []string{`+hello world\r\n`}
+		}
+
+		if data == "OK" {
+			return []string{`+OK\r\n`}
+		}
+	case []string:
+		return []string{`*1\r\n$4\r\nping\r\n`}
 	}
 
-	if data == "OK" {
-		return []string{`+OK\r\n`}
-	}
-	
 	return []string{}
 }
-
 
 // func Deserialize(msg []string) any {}

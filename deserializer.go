d054a13649792ac8ba0e3ac88ec2a812147f71c3
tomprogramuje <tom.severa@outlook.cz>
2024-03-26 08:56:57 +0100

    [GREEN] int test case passes

--- deserializer.go
+++ deserializer.go
@@ -1,9 +1,41 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Deserialize(msg [1]string) any {
-	
-	if msg == [1]string{`+OK\r\n`} {
-		return "OK"
+
+	dataType := string(msg[0][0])
+
+	switch dataType {
+	case "+":
+		withoutPrefix, _ := strings.CutPrefix(msg[0], `+`)
+		string, _ := strings.CutSuffix(withoutPrefix, `\r\n`)
+
+		return string
+
+	case "$":
+		withoutPrefix, _ := strings.CutPrefix(msg[0], `$`)
+		bulkString, _ := strings.CutSuffix(withoutPrefix, `\r\n`)
+
+		if bulkString == "-1" {
+			return nil
+		}
+
+		return bulkString
+
+	case ":":
+		withoutPrefix, _ := strings.CutPrefix(msg[0], `:`)
+		withoutSuffix, _ := strings.CutSuffix(withoutPrefix, `\r\n`)
+
+		number, _ := strconv.Atoi(withoutSuffix)
+
+		return number
+
+	default:
+
+		return nil
 	}
-	return nil
 }

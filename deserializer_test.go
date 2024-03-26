2edcbfb127481cfa8b97ba9dc64afbfee399ee3a
tomprogramuje <tom.severa@outlook.cz>
2024-03-26 08:22:01 +0100

    [RED] added int test case

--- deserializer_test.go
+++ deserializer_test.go
@@ -13,6 +13,7 @@ func TestDeserialize(t *testing.T) {
 	}{
 		{"`$-1\r\n` gets converted to nil", [1]string{`$-1\r\n`}, nil},
 		{"`+OK\r\n` gets converted to 'OK'", [1]string{`+OK\r\n`}, "OK"},
+		{"`:28\r\n` gets converted to 28", [1]string{`:28\r\n`}, 28},
 	}
 
 	for _, test := range cases {
